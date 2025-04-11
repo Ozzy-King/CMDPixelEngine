f16aaa5851fcbc33aa3906d8c66d4a06d501fb1d
Ozzy-King <osbourne.clark14@gmail.com>
2025-04-11 01:55:30 +0100

    added further image support. jpg its smaller. compiles slightly bigger but i feel some compiler setting should fix that

--- Example/WinConsoleGameengine/main.cpp
+++ Example/WinConsoleGameengine/main.cpp
@@ -1,12 +1,8 @@
 #include <iostream>
 #include <Windows.h>
-#include <libloaderapi.h>
 #include <string>
-#include <thread>
-
 #include "GDIEngine.h"
 #include "GDISprite.h"
-#include "world.h"
 
 COLORREF regColour = RGB(255,0,0);
 
@@ -27,45 +23,23 @@ void scrolltext(std::string& str) {
 int main() {
 
 	char title[] = "NULL";
-	if (GDinit(255, 255, 4, 4, title)) {
+	if (GDinit(255, 255, 1, 1, title)) {
 		return 0;
 	}
 	srand(time(NULL));
 	std::cout << "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n" << _GDpixelSetType;
 
-	char fileName[] = "./textTest.bmp";
-	struct GDSPsprite newSorite = GDSPcreateSprite(fileName, 20, 20, 10, 10, 1, RGB(0,0,0));
+	char fileName[] = "./Cards.jpg";
+	struct GDSPsprite newSorite = GDSPcreateSprite(fileName, 1040, 1580, 104, 158, 0, RGB(0,0,0));
 
 	int start = 1;
 	int end = 2;
-
-	//my game stuff
-	world MyWorld;
-	MyWorld.start();
-
-	int tempX = 0; int tempY = 0; int tempZ = 0;
 
 	while (true) { //not good practice
 		GDsetTitle(std::to_string((double)1/ ((double)(end-start)/ CLOCKS_PER_SEC)).c_str());
 		start = (int)clock();
 
-
-		//MyWorld.drawTile(tempX, tempY, tempZ);
-		//tempX++;
-		//if (tempX > 9) {
-		//	tempX = 0;
-		//	tempZ++;
-		//}
-		//if (tempZ > 9) {
-		//	tempZ = 0;
-		//	tempY++;
-		//}
-		//if (tempY > 9) {
-		//	tempY=0;
-		//}
-
-
-		MyWorld.update();
+		GDSPdrawSprite(0, 0, 104, 158, &newSorite, 3);
 
 		GDdrawBackBuffer();
 
@@ -74,7 +48,6 @@ int main() {
 		//std::cin.ignore();
 	}
 
-	MyWorld.destory();
 	GDdeInit();
 
 	std::cin.ignore(); //testing get rid of
